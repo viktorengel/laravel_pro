64f6a7ebf0ef01adfb2178e4eff65cc24151d7ee
Viktor Rengel <viktorengel@hotmail.com>
2023-11-08 23:32:42 -0500

    Ruta view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::view("/","welcome")->name("welcome");
+
+Route::view('/','landing.index')->name('Incio');
+Route::view('/about','landing.about')->name('about');
+
