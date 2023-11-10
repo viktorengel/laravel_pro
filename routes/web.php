b69393de93b4b7c4ec59d4276f0cda46278eb0b5
Viktor Rengel <viktorengel@hotmail.com>
2023-11-09 21:07:31 -0500

    Rutas en los enlaces con el mostash

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/','landing.index')->name('Incio');
 Route::view('/about','landing.about')->name('about');
 
-Route::view('/','index')->name('inicio');
+Route::view('/','index')->name('index');
 Route::view('/about','about')->name('about');
 Route::view('/services','services')->name('services');
 Route::view('/contact','contact')->name('contact');
